2fe5e41e6c745cd43b7e71e32ac0c9ecacb7396b
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-28 03:56:46 -0400

    Update nn_2x2.m

--- nearest-neighbour/nn_2x2.m
+++ nearest-neighbour/nn_2x2.m
@@ -1,32 +1,32 @@
 % 315CA Dinu Ion-Irinel
-function out = nn_2x2(f, STEP = 0.1)
-    % =========================================================================
-    % Aplicarea interpolarii nearest-neighbour pe imaginea 2x2 f cu puncte
-    % intermediare echidistante.
-    % f are valori cunoscute in punctele (1, 1), (1, 2), (2, 1) si (2, 2).
-    % 
-    % Parametrii:
-    % - f = imaginea ce se doreste sa fie interpolata
-    % - STEP = distanta dintre doua puncte succesive
-    % =========================================================================
+function out = nn_2x2 (f, STEP = 0.1)
+    % =============================================== =======================
+    % Applying the nearest-neighbor interpolation to the 2x2 f dotted image
+    % equidistant intermediate.
+    % f has known values in points (1, 1), (1, 2), (2, 1) and (2, 2).
+    %
+    % Parameters:
+    % - f = the image to be interpolated
+    % - STEP = distance between two successive points
+    % =============================================== =======================
     
-    % definirea coordonatelor x si y ale punctelor intermediare
-    x_int = 1 : STEP : 2;
-    y_int = 1 : STEP : 2;
+    % definition of x and y coordinates of intermediate points
+    x_int = 1: STEP: 2;
+    y_int = 1: STEP: 2;
     
-    % determinarea numarului de puncte
-    n = length(x_int);
-    % initializare matrice nxn plina cu zerouri
-    matrix = zeros(n,n);
-    % parcurgerea fiecarui pixel din imaginea finala
-    for i = 1 : n
-        for j = 1 : n 
-            % aflarea celui mai apropiat vecin folosind functia round
-            % x_p si y_p vor fi aproximate
-            x_p = round([x_int(i)]);
-            y_p = round([y_int(j)]); 
-            % determinarea si calcularea pixelul-ui din imaginea rezultata
-            matrix(i, j) = f(x_p, y_p);
+    % determination of the number of points
+    n = length (x_int);
+    % nxn matrix initialization full of zeros
+    matrix = zeros (n, n);
+    % scrolling through each pixel in the final image
+    for i = 1: n
+        for j = 1: n
+            % find the nearest neighbor using the round function
+            % x_p and y_p will be approximated
+            x_p = round ([x_int (i)]);
+            y_p = round ([y_int (j)]);
+            % determination and calculation of the pixel in the resulting image
+            matrix (i, j) = f (x_p, y_p);
         endfor
     endfor
     out = matrix;
