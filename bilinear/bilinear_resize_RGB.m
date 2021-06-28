e296a4dabcba36c766c2360595ed67ed7e4702ba
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-28 04:01:14 -0400

    Update bilinear_resize_RGB.m

--- bilinear/bilinear_resize_RGB.m
+++ bilinear/bilinear_resize_RGB.m
@@ -1,24 +1,24 @@
 % 315CA Dinu Ion-Irinel
-function out = bilinear_resize_RGB(img, p, q)
-    % =========================================================================
-    % Redimensioneaza imaginea img a.i. aceasta sa fie de dimensiune p x q.
-    % Imaginea img este colorata.
-    % Practic, apeleaza de 3 ori functia bilinear pe fiecare canal al imaginii.
-    % =========================================================================
+function out = bilinear_resize_RGB (img, p, q)
+    % =============================================== =======================
+    % Resize image img a.i. it should be of size p x q.
+    % The img image is colored.
+    % Basically, it calls the bilinear function 3 times on each channel of the image.
+    % =============================================== =======================
 
-    % extragerea canalului rosu al imaginii
-    red_channel = img(:, :, 1);
-    % extragerea canalului verde al imaginii
-    green_channel = img(:, :, 2); 
-    % extragerea canalului albastru al imaginii
-    blue_channel = img(:, :, 3); 
+    % extraction of the red channel of the image
+    red_channel = img (:,:, 1);
+    % extract the green channel of the image
+    green_channel = img (:,:, 2);
+    % extract the blue channel of the image
+    blue_channel = img (:,:, 3);
 
-    % aplicarea functiei bilinear_resize pentru fiecare canal al imaginii
-    red_matrix = bilinear_resize(red_channel, p, q);
-    green_matrix = bilinear_resize(green_channel, p, q);
-    blue_matrix = bilinear_resize(blue_channel, p, q);
+    % application of the bilinear_resize function for each channel of the image
+    red_matrix = bilinear_resize (red_channel, p, q);
+    green_matrix = bilinear_resize (green_channel, p, q);
+    blue_matrix = bilinear_resize (blue_channel, p, q);
     size = 3;
-    % formarea imaginii finale cu cele 3 canale de culori
-    out = cat(size, red_matrix, green_matrix, blue_matrix);
+    % formation of the final image with the 3 color channels
+    out = cat (size, red_matrix, green_matrix, blue_matrix);
 
 endfunction
