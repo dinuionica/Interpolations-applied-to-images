fb8f2ee28662ff022d8b6c6afb503422ffbe625a
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-28 04:05:40 -0400

    Update fxy.m

--- bicubic/fxy.m
+++ bicubic/fxy.m
@@ -1,17 +1,17 @@
 % 315CA Dinu Ion-Irinel
-function r = fxy(f, x, y)
-    % =========================================================================
-    % Aproximarea derivatei fata de x si y a lui f in punctul (x, y).
-    % =========================================================================
+function r = fxy (f, x, y)
+     % =============================================== =======================
+     % Approximation of the derivative with respect to x and y of f at point (x, y).
+     % =============================================== =======================
      
-    % daca x sau y se afla pe marginea imaginii derivata este considerata 0
-    [m n] = size(f);
-    if x == 1 || x == n || y == 1 || y == m
-        r = 0;
-    else
-        % altfel calculam derivata folosind formula specifica
-        f_xy = (f(y - 1, x - 1) + f(y + 1, x + 1) - f(y - 1, x + 1) - f(y + 1,x - 1)) / 4;
-        r = f_xy;
-    endif
+     % if x or y is on the edge of the derived image is considered 0
+     [m n] = size (f);
+     if x == 1 || x == n || y == 1 || y == m
+         r = 0;
+     else
+         % otherwise we calculate the derivative using the specific formula
+         f_xy = (f (y - 1, x - 1) + f (y + 1, x + 1) - f (y - 1, x + 1) - f (y + 1, x - 1)) / 4;
+         r = f_xy;
+     endif
    
 endfunction
