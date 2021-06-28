65c43576a6fa49acce1074b8ff686a14720e78c7
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-28 04:00:13 -0400

    Update bilinear_coef.m

--- bilinear/bilinear_coef.m
+++ bilinear/bilinear_coef.m
@@ -1,21 +1,21 @@
 % 315CA Dinu Ion-Irinel
-function a = bilinear_coef(f, x1, y1, x2, y2)
-    % =========================================================================
-    % Calcularea coeficientiilor a pentru interpolarea biliniara intre punctele
-    % (x1, y1), (x1, y2), (x2, y1) si (x2, y2)
-    % =========================================================================
-    % determinarea matricii A
-    A = [1, x1, y1, x1 * y1;
-         1, x1 , y2, x1 * y2;
-         1, x2, y1, x2 * y1;
-         1, x2, y2, x2 * y2];
-    % determinarea vectorului b
-    b = [f(y1, x1); f(y2, x1); f(y1, x2); f(y2, x2)];
-    % convertirea vectorului b la double
-    b = double(b);
+function a = bilinear_coef (f, x1, y1, x2, y2)
+     % =============================================== =======================
+     % Calculation of the coefficients a for bilinear interpolation between points
+     % (x1, y1), (x1, y2), (x2, y1) if (x2, y2)
+     % =============================================== =======================
+     % determination of the matrix A
+     A = [1, x1, y1, x1 * y1;
+          1, x1, y2, x1 * y2;
+          1, x2, y1, x2 * y1;
+          1, x2, y2, x2 * y2];
+     % determination of the vector b
+     b = [f (y1, x1); f (y2, x1); f (y1, x2); f (y2, x2)];
+     % convert vector b to double
+     b = double (b);
       
-    % calcularea coeficientiilor
-    coef = A \ b;
-    a = coef;
+     % calculation of coefficients
+     coef = A \ b;
+     a = coef;
 
 endfunction
