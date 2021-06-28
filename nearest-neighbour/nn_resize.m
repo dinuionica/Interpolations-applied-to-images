990a484981100cf3f04498b7acbd16ead9d2ef75
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-28 03:57:57 -0400

    Update nn_resize.m

--- nearest-neighbour/nn_resize.m
+++ nearest-neighbour/nn_resize.m
@@ -1,50 +1,50 @@
 % 315CA Dinu Ion-Irinel
-function R = nn_resize(I, p, q)
-    % =========================================================================
-    % Upscaling de imagine folosind algoritmul de interpolare nearest-neighbour
-    % Transforma imaginea I din dimensiune m x n in dimensiune p x q
-    % =========================================================================
+function R = nn_resize (I, p, q)
+    % =============================================== =======================
+    % Image upscaling using the nearest-neighbor interpolation algorithm
+    % Transforms image I from size m x n to size p x q
+    % =============================================== =======================
     
-    [m n nr_colors] = size(I);
+    [m n nr_colors] = size (I);
 
-    % convertirea imaginii de intrare la alb-negru daca este cazul
-    if nr_colors > 1
+    % convert the input image to black and white if necessary
+    if nr_colors> 1
         R = -1;
         return
     endif
     
-    % cast la double pentru I
-    I = double(I);
-    % initiliazare matrice finala
-    R = zeros(p, q);
-    % determinarea factorilor de scalare
+    % cast to double for I
+    I = double (I);
+    % final matrix initialization
+    R = zeros (p, q);
+    % determination of scaling factors
     s_x = (q - 1) / (n - 1);
     s_y = (p - 1) / (m - 1);
    
-    % definirea matricei de transformare pentru redimensionare
+    % definition of the transformation matrix for resizing
     T = [s_x, 0; 0, s_y];
-    % calculare matrice inversa a transformarii
-    T_INV = [1 / s_x, 0; 0, 1 /s_y];
+    % inverse matrix calculation of the transformation
+    T_INV = [1 / s_x, 0; 0, 1 / s_y];
     
-    % parcurgerea fiecareui pixel din imagine
-    for y = 0 : p - 1
-        for x = 0 : q - 1
-          % determinare x_p si y_p din spatiul imaginii initiale
+    % scroll through each pixel in the image
+    for y = 0: p - 1
+        for x = 0: q - 1
+          % determination of x_p and y_p in the initial image space
             point = T_INV * [x; y];
-            x_p = point(1);
-            y_p = point(2);
-            % trecea (xp, yp) din sistemul de coordonate de la 0 la n - 1 in
-            % sistemul de coordonate de la 1 la n pentru a aplica interpolarea
+            x_p = point (1);
+            y_p = point (2);
+            % passed (xp, yp) from the coordinate system from 0 to n - 1 in
+            % coordinate system from 1 to n to apply interpolation
             x_p = x_p + 1;
             y_p = y_p + 1;
-            % calcularea celui mai apropiat vecin folosind functia round
-            x_f = round([x_p]);
-            y_f = round([y_p]);
-            % calcularea valoarii pixelului din imaginea finala
-            R(y + 1, x + 1) = I(y_f, x_f);
+            % calculation of the nearest neighbor using the round function
+            x_f = round ([x_p]);
+            y_f = round ([y_p]);
+            % calculating the pixel value in the final image
+            R (y + 1, x + 1) = I (y_f, x_f);
         endfor
     endfor
-    % convertire matrice rezultat la uint8
-    R = uint8(R);
+    % result matrix conversion to uint8
+    R = uint8 (R);
 
 endfunction
