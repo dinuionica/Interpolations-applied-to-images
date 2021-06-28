e1c024f8f9342152f30b98912c5ad5c989d87e52
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-28 04:06:06 -0400

    Update fy.m

--- bicubic/fy.m
+++ bicubic/fy.m
@@ -1,17 +1,17 @@
 % 315CA Dinu Ion-Irinel
-function r = fy(f, x, y)
-    % =========================================================================
-    % Aproximarea derivatei fata de y a lui f in punctul (x, y).
-    % =========================================================================
+function r = fy (f, x, y)
+     % =============================================== =======================
+     % Approximation of the derivative to y of f at point (x, y).
+     % =============================================== =======================
 
-    % daca y se afla pe marginea imaginii derivata este considerata 0
-    [m n] = size(f);
-    if y == 1 || y == m 
-        r = 0;
-    else
-        % altfel calculam derivata folosind formula specifica
-        f_y = (f(y + 1,x) - f(y - 1,x)) / 2;
-        r = f_y;
-    endif
+     % if y is on the edge of the derived image is considered 0
+     [m n] = size (f);
+     if y == 1 || y == m
+         r = 0;
+     else
+         % otherwise we calculate the derivative using the specific formula
+         f_y = (f (y + 1, x) - f (y - 1, x)) / 2;
+         r = f_y;
+     endif
 
 endfunction
