ce8ac388fd2e820cb85d581978cef4c0b69ded2e
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-28 04:03:35 -0400

    Update bicubic_coef.m

--- bicubic/bicubic_coef.m
+++ bicubic/bicubic_coef.m
@@ -1,30 +1,30 @@
 % 315CA Dinu Ion-Irinel
-function A = bicubic_coef(f, Ix, Iy, Ixy, x1, y1, x2, y2)
-    % =========================================================================
-    % Calcularea coeficientiilor de interpolare bicubica pentru 4 puncte alaturate
-    % =========================================================================
+function A = bicubic_coef (f, Ix, Iy, Ixy, x1, y1, x2, y2)
+    % ======================================================================
+    % Calculation of bicubic interpolation coefficients for 4 adjacent points
+    % ======================================================================
 
-    % calcularea matricelor intermediare
-    matrix1 = [1, 0, 0, 0; 
+    % calculation of intermediate matrices
+    matrix1 = [1, 0, 0, 0;
                0, 0, 1, 0;
-              -3, 3,-2,-1;
-               2,-2, 1, 1];
+              -3, 3, -2, -1;
+               2, -2, 1, 1];
 
     matrix2 = [1, 0, -3, 2;
                0, 0, 3, -2;
                0, 1, -2, 1;
                0, 0, -1, 1];
     
-    derived_matrix = [f(y1, x1), f(y2,x1), Iy(y1, x1), Iy(y2, x1);
-                      f(y1,x2), f(y2,x2), Iy(y1,x2), Iy(y2,x2);  
-                      Ix(y1, x1), Ix(y2, x1), Ixy(y1, x1), Ixy(y2,x1);
-                      Ix(y1, x2), Ix(y2, x2), Ixy(y1, x2), Ixy(y2, x2)];
-    % convertire matrici intermediare la double
-      matrix1 = double(matrix1);
-      matrix2 = double(matrix2);
-      derived_matrix  = double(derived_matrix );
+    derived_matrix = [f (y1, x1), f (y2, x1), Iy (y1, x1), Iy (y2, x1);
+                      f (y1, x2), f (y2, x2), Iy (y1, x2), Iy (y2, x2);
+                      Ix (y1, x1), Ix (y2, x1), Ixy (y1, x1), Ixy (y2, x1);
+                      Ix (y1, x2), Ix (y2, x2), Ixy (y1, x2), Ixy (y2, x2)];
+    % convert arrays to double
+      matrix1 = double (matrix1);
+      matrix2 = double (matrix2);
+      derived_matrix = double (derived_matrix);
       
-    % obtinerea matricii finale
+    % obtaining the final matrix
       A = matrix1 * derived_matrix * matrix2;
       
 endfunction
