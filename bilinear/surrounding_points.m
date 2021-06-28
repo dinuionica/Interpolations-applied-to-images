82af7c054ae1899dee59f82fa64e0595f3af429d
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-28 04:03:04 -0400

    Update surrounding_points.m

--- bilinear/surrounding_points.m
+++ bilinear/surrounding_points.m
@@ -1,25 +1,25 @@
 % 315CA Dinu Ion-Irinel
-function [x1 y1 x2 y2] = surrounding_points(n, m, x, y)
-    % =========================================================================
-    % Calcularea celor 4 puncte ce contin in interior (x, y)
-    % Primeste si dimensiunea unei imagini m x n, pentru a asigura ca nu se
-    % iese din matrice.
-    % =========================================================================
+function [x1 y1 x2 y2] = surrounding_points (n, m, x, y)
+     % ======================================================================
+     % Calculation of the 4 points containing inside (x, y)
+     % Also receives the size of an image m x n, to ensure that it is not
+     % exits the matrix.
+     % ======================================================================
     
-    % calculare x1, y1, x2, y2
-    x1 = floor(x);
-    x2 = floor(x) + 1;
-    y1 = floor(y);
-    y2 = floor(y) + 1;
+     % calculate x1, y1, x2, y2
+     x1 = floor (x);
+     x2 = floor (x) + 1;
+     y1 = floor (y);
+     y2 = floor (y) + 1;
 
-    % daca y1 se afla pe ultima linie y2 nu trebuie sa iasa din matrice
-    if y1 == m 
-      y1 = y1 - 1;
-      y2 = y2 - 1;
-    endif
-    % daca x1 se afla pe ultima coloana x2 nu trebuie sa iasa din matrice
-    if x1 == n 
-      x1 = x1 - 1;
-      x2 = x2 - 1;
-    endif
+     % if y1 is on the last line y2 must not exit the matrix
+     if y1 == m
+       y1 = y1 - 1;
+       y2 = y2 - 1;
+     endif
+     % if x1 is on the last column x2 must not exit the matrix
+     if x1 == n
+       x1 = x1 - 1;
+       x2 = x2 - 1;
+     endif
 endfunction
