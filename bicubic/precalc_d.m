1dcd6b2d08344a0cf85502bc3f5b74839b58e35b
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-28 04:06:27 -0400

    Update precalc_d.m

--- bicubic/precalc_d.m
+++ bicubic/precalc_d.m
@@ -1,27 +1,26 @@
 % 315CA Dinu Ion-Irinel
-function [Ix, Iy, Ixy] = precalc_d(I)
-    % =========================================================================
-    % Prelucrarea matricilor Ix, Iy si Ixy ce contin derivatele dx, dy, dxy ale 
-    % imaginii I pentru fiecare pixel al acesteia
-    % =========================================================================
+function [Ix, Iy, Ixy] = precalc_d (I)
+     % =============================================== =======================
+     % Processing of matrices Ix, Iy and Ixy containing the derivatives dx, dy, dxy of
+     % of image I for each pixel of it
+     % =============================================== =======================
     
-    % determinare dimensiuni imaginii
-    [m n] = size(I);
+     % image size determination
+     [m n] = size (I);
     
-    % convertire matrice I la double
-      I = double(I);
-    % parcurgerea fiecarui pixel din imagine
-      for y = 1 : m
-        for x = 1 : n
-          % determinare matrice cu derivate fata de x Ix
-          Ix(y,x) = fx(I, x, y);
-          % determinare matrice cu derivate fata de y Iy
-          Iy(y,x) = fy(I, x, y);
-          % determinare matrice cu derivate fata de xy Ixy
-          Ixy(y,x) = fxy(I, x, y);
+     % convert matrix I to double
+       I = double (I);
+     % scroll through each pixel in the image
+       for y = 1: m
+         for x = 1: n
+           % matrix determination with derivatives with respect to x Ix
+           Ix (y, x) = fx (I, x, y);
+           % matrix determination with derivatives with respect to y Iy
+           Iy (y, x) = fy (I, x, y);
+           % matrix determination with derivatives with respect to xy Ixy
+           Ixy (y, x) = fxy (I, x, y);
 
-        endfor
-      endfor
+         endfor
+       endfor
       
 endfunction
-
